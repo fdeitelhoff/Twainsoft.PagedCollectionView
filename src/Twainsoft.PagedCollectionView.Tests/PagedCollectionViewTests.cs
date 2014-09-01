1d3eb1e90a4bf137ede1978e3ec516fd0c969df5
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-09-01 16:06:39 +0200

    Trying to add Machine Specifications.

--- src/Twainsoft.PagedCollectionView.Tests/PagedCollectionViewTests.cs
+++ src/Twainsoft.PagedCollectionView.Tests/PagedCollectionViewTests.cs
@@ -1,6 +1,18 @@
-﻿namespace Twainsoft.PagedCollectionView.Tests
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+
+namespace Twainsoft.PagedCollectionView.Tests
 {
-    public class PagedCollectionViewTests
+    [Subject(typeof(Data.PagedCollectionView), "")]
+    public class When_a_new_paged_collection_view_is_filled_with_data
     {
+        static Data.PagedCollectionView subject;
+
+        Establish context = () =>
+        {
+            subject = new Data.PagedCollectionView(new List<string> {"1", "2", "3"});
+        };
+
+
     }
 }
