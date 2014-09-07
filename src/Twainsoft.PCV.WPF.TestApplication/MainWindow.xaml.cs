52e150c5c2c251b72af2ea84901ebdcde63b09de
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-09-07 13:17:28 +0200

    Some changes (paging) within the WPF test application.

--- src/Twainsoft.PCV.WPF.TestApplication/MainWindow.xaml.cs
+++ src/Twainsoft.PCV.WPF.TestApplication/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using Twainsoft.PCV.Data;
 using Twainsoft.PCV.WPF.TestApplication.Model;
@@ -8,6 +7,8 @@ namespace Twainsoft.PCV.WPF.TestApplication
 {
     public partial class MainWindow
     {
+        private PagedCollectionView ArticleCollectionView { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -15,34 +16,52 @@ namespace Twainsoft.PCV.WPF.TestApplication
             var data = new ObservableCollection<Article> { new Article("Test 1", "Author 1", 3),
                 new Article("Test 2", "Author 2", 3.3f), new Article("Test 3", "Author 3", 17.9f) };
 
-            var pagedCollectionView = new PagedCollectionView(data) { PageSize = 1 };
+            ArticleCollectionView = new PagedCollectionView(data) { PageSize = 1 };
 
-            DataGrid.ItemsSource = pagedCollectionView;
+            DataGrid.ItemsSource = ArticleCollectionView;
         }
 
         private void SearchOnClick(object sender, RoutedEventArgs e)
         {
-            
+            if (string.IsNullOrWhiteSpace(Search.Text))
+            {
+                ArticleCollectionView.Filter = null;
+            }
+            else
+            {
+                ArticleCollectionView.Filter += delegate(object item)
+                {
+                    var article = item as Article;
+
+                    if (article == null)
+                    {
+                        return false;
+                    }
+
+                    return article.Author.ToLower().Contains(Search.Text.ToLower())
+                           || article.Name.ToLower().Contains(Search.Text.ToLower());
+                };
+            }
         }
 
         private void LastPageOnClick(object sender, RoutedEventArgs e)
         {
-         
+            ArticleCollectionView.MoveToLastPage();
         }
 
         private void FirstPageOnClick(object sender, RoutedEventArgs e)
         {
-            
+            ArticleCollectionView.MoveToFirstPage();
         }
 
         private void PreviousPageOnClick(object sender, RoutedEventArgs e)
         {
-            
+            ArticleCollectionView.MoveToPreviousPage();
         }
 
         private void NextPageOnClick(object sender, RoutedEventArgs e)
         {
-            
+            ArticleCollectionView.MoveToNextPage();
         }
     }
 }
